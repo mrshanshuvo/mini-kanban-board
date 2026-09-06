137aef1d22d1e40a9ca0741bb6105090c1020648
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2026-09-06 21:49:03 +0600

    add prettier formatter and configure eslint rules in frontend

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -13,6 +13,17 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
